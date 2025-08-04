aca2d296114967fbba1d06f6ba61d34bf73146db
VictorAkashReznov <akashkushwaha5956@gmail.com>
2025-08-04 20:43:39 +0530

    okay every thing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://expense-tracker-backend-plum-eight.vercel.app",
+    "https://expense-tracker-backend-1guigoaau.vercel.app",
   timeout: 10000,
 });
 
